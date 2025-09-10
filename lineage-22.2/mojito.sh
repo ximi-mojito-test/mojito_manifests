0586c4dd02e8c86068870946230dd3d0ac77b46d
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-09-11 07:47:44 +0800

    manifest

---
+++ lineage-22.2/mojito.sh
@@ -0,0 +1,9 @@
+git clone -b personal https://github.com/ximi-mojito-test/device_xiaomi_mojito --depth=1 device/xiaomi/mojito
+
+git clone -b personal https://github.com/ximi-mojito-test/mojito_krenol/ --depth=1 kernel/xiaomi/mojito
+
+git clone -b mojito-voyager https://github.com/ximi-mojito-test/android_device_xiaomi_sm6150-common --depth=1 device/xiaomi/sm6150-common
+
+git clone -b 15 https://github.com/ximi-mojito-test/android_vendor_xiaomi_mojito --depth=1 vendor/xiaomi/mojito
+
+git clone -b 15 https://github.com/ximi-mojito-test/android_vendor_xiaomi_sm6150-common --depth=1 vendor/xiaomi/sm6150-common
