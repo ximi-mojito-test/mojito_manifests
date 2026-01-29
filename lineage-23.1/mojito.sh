ca5a1c1385e462c4ff1ba4bc9c474b48cceced0b
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-01-29 19:55:45 +0800

    Update mojito.sh

--- lineage-23.1/mojito.sh
+++ lineage-23.1/mojito.sh
@@ -15,3 +15,4 @@ git clone -b lineage-23.1-tweaked https://github.com/ximi-daisy-test/android_boo
 git clone -b lineage-23.1-tweaked https://github.com/ximi-daisy-test/android_vendor_lineage --depth=1 vendor/lineage
 
 git clone -b lineage-23.1 https://github.com/LineageOS/android_hardware_xiaomi --depth=1 hardware/xiaomi
+
